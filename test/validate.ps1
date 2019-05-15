7ecb4e3cc100b0be70faf4ccf0187050470898e5
Bernard <bernard@ducourier.com>
2019-05-15 14:38:38 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -23,7 +23,7 @@ $currentBranch = git rev-parse --abbrev-ref HEAD
 
 if ($currentBranch -eq 'master') {
     $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
-    if ($confirmation -eq 'n') {
+    if ($confirmation -ne 'y') {
         exit
     }
 }
@@ -35,11 +35,12 @@ git add . ; git commit -m "Update validation" ; git push origin $currentBranch
 Select-AzureRmSubscription -Subscription $subscription
 
 # Cleanup validation resource content in case it did not properly completed and left over components are still lingeringcd
-Write-Host "Cleanup validation resource content...";
+Write-Host "Cleanup old $templateLibraryName validation resources if needed...";
+
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\parameters\cleanup.json") -Force -Verbose
 
 # Start the deployment
-Write-Host "Starting validation deployment...";
+Write-Host "Starting $templateLibraryName dependancies deployment...";
 
 New-AzureRmDeployment -Location $Location -Name "Deploy-Infrastructure-Dependancies" -TemplateUri "https://raw.githubusercontent.com/canada-ca-azure-templates/masterdeploy/20190514/template/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -Verbose;
 
@@ -51,6 +52,8 @@ if ($provisionningState -eq "Failed") {
 }
 
 # Validating server template
+Write-Host "Starting $templateLibraryName validation deployment...";
+
 $validationURL = getValidationURL
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template" -TemplateUri $validationURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\parameters\validate.parameters.json") -Verbose
 
@@ -61,5 +64,5 @@ if ($provisionningState -eq "Failed") {
 }
 
 # Cleanup validation resource content
-Write-Host "Cleanup validation resource content...";
+Write-Host "Cleanup $templateLibraryName validation resources...";
 New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Mode Complete -TemplateFile (Resolve-Path "$PSScriptRoot\parameters\cleanup.json") -Force -Verbose
